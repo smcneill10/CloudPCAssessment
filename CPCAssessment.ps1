e76a076d8efc68c2431d564a91e0675f5a0546ba
Sean McNeill <109704964+smcneill10@users.noreply.github.com>
2024-06-24 20:14:44 -0600

    more v1 setup stuff

--- CPCAssessment.ps1
+++ CPCAssessment.ps1
@@ -1,17 +1,19 @@
-# Need to have PowerShell 7 or higher https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.3
+
 # https://techcommunity.microsoft.com/t5/windows-it-pro-blog/now-available-microsoft-graph-windows-365-apis/ba-p/4094550
 # https://developer.microsoft.com/en-us/graph/changelog
 # https://learn.microsoft.com/en-us/graph/use-the-api?context=graph%2Fapi%2Fbeta&view=graph-rest-beta#version
 
+# https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell?view=powershell-7.3
+# https://learn.microsoft.com/en-us/powershell/microsoftgraph/installation?view=graph-powershell-1.0
+# https://learn.microsoft.com/en-us/powershell/microsoftgraph/get-started?view=graph-powershell-1.0
+
 #Install the required modules
-Install-Module -Name Microsoft.Graph.Authentication -Force
-Install-Module -Name Microsoft.Graph.PowerShell -Force
-install-Module -Name Microsoft.Graph.DeviceManagement.Administration -Force
+Install-Module -Name Microsoft.Graph -Force -AllowClobber
 
 #Connect to CloudPC Graph API 
 Connect-MgGraph -Scopes "CloudPC.ReadWrite.All, User.Read.All","Group.Read.All, CloudPC.read.all"
 #Set Graph API to Beta or V1.0
-#Select-MgProfile Beta
+#Select-MgProfile v1.0
 
 #Gathers the connection info, comment out the Clear-Host line below to see this info, helps with connectivity issues
 Write-host "Here is the connection information used:" #-BackgroundColor $BKColorInfo -ForegroundColor $FGColor
